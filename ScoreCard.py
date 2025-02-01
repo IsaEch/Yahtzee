5104a93cb0cc6c11e9009bdff7b4a62912b0f586
Isaiah Echeverria <iecheverria@gardner-webb.edu>
2025-02-01 16:12:13 -0500

    Correct version of scorecard with section names and buttons with hover color to know what to select
    Signed-off-by: Isaiah Echeverria <Iecheverria@gardner-webb.edu>

--- ScoreCard.py
+++ ScoreCard.py
@@ -1,11 +1,16 @@
 class ScoreCard(object):
-    row_descriptions = {0: "Aces", 1: "Twos", 2: "Threes", 3: "Fours", 4: "Fives", 5: "Sixes", 6: "TOTAL SCORE",
-                        7: "BONUS", 8: "TOTAL"}
+    row_descriptions = {0: "Upper Section", 1: "Aces", 2: "Twos", 3: "Threes", 4: "Fours", 5: "Fives", 6: "Sixes",
+                        7: "TOTAL SCORE", 8: "BONUS", 9: "TOTAL", 10: "Lower Section", 11: "3 of a kind",
+                        12: "4 of a kind", 13: "Full House", 14: "Small Straight", 15: "Large Straight", 16: "Yahtzee",
+                        17: "Chance", 18: "Yahtzee Bonus", 19: "TOTAL: Upper", 20: "TOTAL: Lower",
+                        21: "GRAND TOTAL"}
     def __init__(self):
-        self.score = [[0]*6 for _ in range(13)]
-        for n in range(13):
-            if n < 9:
+        self.score = [[0]*6 for _ in range(22)]
+        for n in range(22):
+            if n < 22:
                 self.score[n][0] = ScoreCard.row_descriptions[n]
+            if n < 5:
+                self.score[0][n+1] = n+1
 
     def length(self):
         return len(self.score)
--- YahtzeeGame.py
+++ YahtzeeGame.py
@@ -1,5 +1,7 @@
 from Player import Player
 import Constants as C
+from Text import Text
+from Button import Button
 import pygame
 
 
@@ -11,10 +13,15 @@ class YahtzeeGame(object):
         self.game_started = False
         self.game_finished = False
         self.screen = pygame.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))
+        self.game_name = Text("YAHTZEE", C.FONT_SIZE,  C.BLACK_COLOR, C.YAHTZEE_X, C.YAHTZEE_Y)
+        self.player_name = Text(self.players[self.current_player].name, C.FONT_SIZE, C.BLACK_COLOR, C.PLAYER_X,
+                                C.PLAYER_Y)
         pygame.display.set_caption(C.CAPTION)
         pygame.display.background = C.BACKGROUND_COLOR
+        self.buttons = []
 
     def start_game(self):
+        self.create_buttons()
         self.game_started = True
         self.screen.fill(C.BACKGROUND_COLOR)
         while self.game_started:
@@ -28,18 +35,24 @@ class YahtzeeGame(object):
         # Local variable to hold the current score_card to iterate through
         card = self.players[self.current_player].score_card
         # Draw the score_card
-        pygame.draw.rect(self.screen, C.SCORE_CARD_COLOR, (C.SCORE_CARD_X_Y, C.SCORE_CARD_X_Y, C.SCREEN_WIDTH//2,
-                                                           C.SCREEN_HEIGHT-20))
+        pygame.draw.rect(self.screen, C.SCORE_CARD_COLOR, (C.SCORE_CARD_X, C.SCORE_CARD_Y, C.SCREEN_WIDTH//2,
+                                                           C.SCREEN_HEIGHT - C.SC_BACKGROUND_OFFSET))
 
         # Draw the grid
-        for row in range(card.length()):
-            grid_y = C.CELL_HEIGHT * row + C.SCORE_CARD_OFFSET
-            pygame.draw.line(self.screen, C.BLACK_COLOR, (C.SCORE_CARD_X_Y, grid_y),
-                             (C.SCREEN_WIDTH//2 + C.SCORE_CARD_X_Y, grid_y))
-        for col in range(card.length()):
-            grid_x = C.CELL_WIDTH * col + C.SCORE_CARD_OFFSET if col == 0 else C.CELL_WIDTH * col + C.SC_DESC_OFFSET
-            pygame.draw.line(self.screen, C.BLACK_COLOR, (grid_x, C.SCORE_CARD_X_Y),
-                             (grid_x, C.SCREEN_HEIGHT + C.SCORE_CARD_X_Y))
+        for row in range(card.length() + 3):
+            grid_y = C.CELL_HEIGHT * row + (C.SCORE_CARD_OFFSET*2 - C.SC_X_OFFSET)
+            pygame.draw.line(self.screen, C.BLACK_COLOR, (C.SCORE_CARD_X + C.SC_GRID_OFFSET, grid_y),
+                         (C.SCREEN_WIDTH//2 + C.SCORE_CARD_Y-C.GRID_Y_OFFSET, grid_y))
+
+        for col in range(card.width() + 1):
+            # 1st column has a different offset because it needs more room for text
+            if col == 0:
+                grid_x = C.SCORE_CARD_X + C.SCORE_CARD_OFFSET - C.COL_ONE_OFFSET
+            else:
+                grid_x = C.SCORE_CARD_X + C.SCORE_CARD_OFFSET + col * (C.SCREEN_WIDTH//2 - 2 * C.SCORE_CARD_OFFSET) // card.width()
+            # Adjust line length to fit the card
+            pygame.draw.line(self.screen, C.BLACK_COLOR, (grid_x, C.SCORE_CARD_X+C.SC_BEGIN_LENGTH_OFFSET),
+                             (grid_x, C.SCREEN_HEIGHT + C.SCORE_CARD_Y - C.SC_END_LENGTH_OFFSET))
 
         # Draw the score_card values
         for row in range(card.length()):
@@ -49,10 +62,45 @@ class YahtzeeGame(object):
                 # Render the text
                 cell_text = C.FONT.render(cell_value, True, C.BLACK_COLOR)
                 # Draw each cell value on the screen and push text to make space for the 1st column
-                x = C.CELL_WIDTH * col + C.SCORE_CARD_OFFSET if col == 0 else (C.CELL_WIDTH * col
-                                                                               + C.SC_DESC_OFFSET)
-                y = C.CELL_HEIGHT * row + C.SCORE_CARD_OFFSET
+                if col == 0:
+                    x = C.CELL_WIDTH * col + C.SCORE_CARD_OFFSET - C.COL_ONE_OFFSET
+                elif row == 10:
+                    continue
+                else:
+                    # Adjust the x position to fit the card
+                    x = (C.SCORE_CARD_X + C.SCORE_CARD_OFFSET + col * (C.SCREEN_WIDTH//2 - 2 * C.SCORE_CARD_OFFSET)
+                         // card.width())
+                    x += C.COLS_OFFSET
+                x += C.COLUMN_OFFSET
+                y = C.CELL_HEIGHT * row + C.SCORE_CARD_OFFSET + C.VALUES_OFFSET
                 self.screen.blit(cell_text, (x, y))
+
+        # Draw the yahtzee name
+        self.game_name.draw(self.screen)
+        # Draw the player name
+        self.player_name.draw(self.screen)
+        # Draw the buttons on the screen
+        for button in self.buttons:
+            if button.is_hovered():
+                button.color = C.HOVER_COLOR
+            else:
+                button.color = C.WHITE_COLOR
+            button.draw(self.screen)
+
+    def create_buttons(self):
+        """Creates the buttons that the user can use to select the category for each round"""
+        # Local variables to hold the x and y coordinates for the buttons
+        x = C.SCORE_CARD_X + C.SCORE_CARD_OFFSET - C.BUTTON_X_OFFSET
+        y = C.SCORE_CARD_Y + C.SCORE_CARD_OFFSET + C.BUTTON_Y_OFFSET
+        # Creates 13 buttons for the 13 rounds/categories
+        for n in range(13):
+            b = Button(x, y, C.BUTTON_WIDTH, C.CELL_HEIGHT, "", C.FONT, C.WHITE_COLOR, C.WHITE_COLOR)
+            # Moves the button down to the next section since the totals are not clicked
+            if n == 5:
+                y += C.SPLIT
+            else:
+                y += C.CELL_HEIGHT
+            self.buttons.append(b)
 
 
 if __name__ == "__main__":
